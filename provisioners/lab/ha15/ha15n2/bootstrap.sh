7fc7caf398e787a3547004bd23318f663eaa2311
ZeTopHat <colinhamilton@protonmail.com>
2023-03-09 16:16:11 -0700

    Splitting the ha lab into an ha12 lab and an ha15 lab.

--- provisioners/lab/ha15/ha15n2/bootstrap.sh
+++ provisioners/lab/ha15/ha15n2/bootstrap.sh
@@ -5,7 +5,7 @@ DEPLOY=$2
 
 echo "Deploying ${MACHINE} ${DEPLOY} configurations..."
 
-if [ "$MACHINE" == "ha12n2" ]; then
+if [ "$MACHINE" == "ha15n2" ]; then
   SUSEConnect -r $HAREGCODE -p $HAPRODUCT
   echo "StrictHostKeyChecking no" >>/etc/ssh/ssh_config
   mkdir /root/.ssh
@@ -20,28 +20,29 @@ if [ "$MACHINE" == "ha12n2" ]; then
   chown root:root /root/.ssh/id_rsa.pub
   zypper install -y open-iscsi lsscsi cron xfsprogs nfs-kernel-server nfs-client
   zypper install -y -t pattern ha_sles
-  echo "192.168.0.121 ha12n1.labs.suse.com ha12n1" >>/etc/hosts
-  echo "192.168.0.150 haiscsi.labs.suse.com haiscsi" >>/etc/hosts
-  echo "InitiatorName=iqn.2019-20.com.suse.labs.ha12n2:ha12n2" >/etc/iscsi/initiatorname.iscsi
+  echo "192.168.0.151 ha15n1.labs.suse.com ha15n1" >>/etc/hosts
+  echo "192.168.0.150 ha15iscsi.labs.suse.com ha15iscsi" >>/etc/hosts
+  echo "InitiatorName=iqn.2022-08.com.suse.labs.ha15n2:initiator02" >/etc/iscsi/initiatorname.iscsi
   echo "node.session.auth.authmethod = CHAP" >>/etc/iscsi/iscsid.conf
   echo "node.session.auth.username = username" >>/etc/iscsi/iscsid.conf
   echo "node.session.auth.password = password" >>/etc/iscsi/iscsid.conf
-  sed -i 's/node.startup = manual/node.startup = automatic/g' /etc/iscsi/iscsid.conf
+  sed -i 's/node.startup = manual/node.startup = automatic/g' /etc/iscsi/iscsid.conf 
   systemctl enable --now iscsi iscsid
   iscsiadm -m discovery -t sendtargets -p 192.168.0.150
-  iscsiadm --mode node --target iqn.2019-12.com.suse.labs.haiscsi:ha12 --portal haiscsi.labs.suse.com:3260 -o new
+  iscsiadm --mode node --target iqn.2022-08.com.suse.labs.ha15iscsi:ha15 --portal ha15iscsi.labs.suse.com:3260 -o new
   systemctl restart iscsi iscsid
   # these need to be done after iscsi and softdog to avoid issues when the kernel updates
-  zypper install -y --oldpackage dlm-kmp-default$(rpm -q kernel-default | grep -Eo '\-[0-9.-]+') libdlm ocfs2-kmp-default$(rpm -q kernel-default | grep -Eo '\-[0-9.-]+')
+  zypper install -y --oldpackage dlm-kmp-default$(rpm -q kernel-default | grep -Eo '\-[0-9.-]+') ocfs2-kmp-default$(rpm -q kernel-default | grep -Eo '\-[0-9.-]+')
   if [ "$DEPLOY" == "training" ]; then
     echo "training"
   elif [ "$DEPLOY" == "fulldeploy" ]; then
     echo "fulldeploy"
     echo "softdog" > /etc/modules-load.d/watchdog.conf
     systemctl restart systemd-modules-load
+    sed -i 's/use_lvmlockd = 0/use_lvmlockd = 1/' /etc/lvm/lvm.conf
+    systemctl enable --now lvmlockd
     mkdir /shared
     mkdir /data
-    mkdir -p /exports/data2
   else
     echo "Deployment not recognized."
   fi
