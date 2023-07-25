d8cd96da2d3b2c6323c14b6cc99afa6ae8fc44bf
Colin Hamilton <colinhamilton@protonmail.com>
2023-07-25 17:10:31 -0600

    Update bootstrap.ps1
    remove comments

--- provisioners/lab/auth/authdc01/bootstrap.ps1
+++ provisioners/lab/auth/authdc01/bootstrap.ps1
@@ -1,5 +1,5 @@
 Write-Host "Installing AD packages and tools.."
-#Install-WindowsFeature -Name "AD-Domain-Services" -IncludeManagementTools
+Install-WindowsFeature -Name "AD-Domain-Services" -IncludeManagementTools
 Write-Host "Configuring and starting AD services.."
-#$secured = ConvertTo-SecureString 'Windows2016' -AsPlainText -Force
-#Install-ADDSForest -DomainName 'labs.suse.com' -CreateDnsDelegation:$false -DomainMode '7' -DomainNetbiosName 'LABS' -ForestMode '7' -InstallDNS -SafeModeAdministratorPassword $secured -SkipPreChecks -Force
+$secured = ConvertTo-SecureString 'Windows2016' -AsPlainText -Force
+Install-ADDSForest -DomainName 'labs.suse.com' -CreateDnsDelegation:$false -DomainMode '7' -DomainNetbiosName 'LABS' -ForestMode '7' -InstallDNS -SafeModeAdministratorPassword $secured -SkipPreChecks -Force
