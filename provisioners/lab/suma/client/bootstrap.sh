cdf9f1a1242895cdaf02c039e7aa56c39a5972a9
slsnow <66100515+slsnow@users.noreply.github.com>
2023-08-16 18:10:31 -0600

    Update bootstrap.sh

--- provisioners/lab/suma/client/bootstrap.sh
+++ provisioners/lab/suma/client/bootstrap.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
+sed -i "s/\$SUBNET/${SUBNET}/g" /tmp/hosts
+cp /tmp/hosts /etc/hosts
+
 if [ -f /etc/zypp/zypp.conf ]; then
     sed -i 's/rpm.install.excludedocs = yes/rpm.install.excludedocs = no/' /etc/zypp/zypp.conf
 fi
-
-systemctl disable --now firewalld
 
 if hostname | grep sap15sp5; then
     rpm -e --nodeps sles-release
@@ -17,6 +18,7 @@ if hostname | grep sap15sp5; then
     SUSEConnect --de-register
     SUSEConnect --cleanup
     rm -rf /etc/zypp/{credentials,services,repos}.d/*
+fi
 
 if hostname | grep sap12sp5; then
     rpm -e --nodeps sles-release
@@ -24,8 +26,24 @@ if hostname | grep sap12sp5; then
     SUSEConnect --de-register
     SUSEConnect --cleanup
     rm -rf /etc/zypp/{credentials,services,repos}.d/*
+fi
+
+if grep -q 'ID=ubuntu' /etc/os-release; then
+    cat > /etc/sudoers.d/zz-sumaclient.conf << EOF
+# https://documentation.suse.com/suma/4.3/en/suse-manager/client-configuration/clients-ubuntu.html
+<user>  ALL=NOPASSWD: /usr/bin/python, /usr/bin/python2, /usr/bin/python3, /var/tmp/venv-salt-minion/bin/python
+EOF
+    chmod 0440 /etc/sudoers.d/zz-sumaclient.conf
+else
+    echo "This is not an Ubuntu system. Exiting."
+fi
 
 echo "StrictHostKeyChecking no" >>/etc/ssh/ssh_config
 
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 systemctl reload sshd
+
+systemctl disable --now firewalld
+sh -c 'echo root:linux | chpasswd'
+
+echo -e "\nClient $(hostname) Provisioning Complete"
